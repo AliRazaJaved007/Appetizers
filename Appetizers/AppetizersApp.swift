b3f5ea246720e1dbfdcd638397a9079eefa3aa76
CodeSprint <alirazajaveed84@gmail.com>
2024-05-06 12:25:47 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Zain on 2024-05-06.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
